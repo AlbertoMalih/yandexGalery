30d6542d1c0b132837f73368cd6416dbfe964f60
urroboros <gu.oleg1@gmail.com>
2018-05-02 09:58:16 +0300

    prepare project to tests

---
+++ app/src/test/java/com/golegion2001/galery/viewModel/AllPhotosViewModelTest.kt
@@ -0,0 +1,9 @@
+package com.golegion2001.galery.viewModel
+
+import com.golegion2001.galery.listPictures.AllPhotosViewModel
+
+class AllPhotosViewModelTest {
+    private lateinit var viewModel: AllPhotosViewModel
+
+
+}
